33b6c9772af8a2133bc8a576f4374ce8ecaa40e2
Danylo Lytovchenko <danylo.lytovchenko@pm.me>
2025-03-10 17:27:10 +0100

    Draw map in C

--- c/main.c
+++ c/main.c
@@ -1,6 +1,138 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_blendmode.h>
+#include <SDL3/SDL_error.h>
+#include <SDL3/SDL_events.h>
+#include <SDL3/SDL_init.h>
+#include <SDL3/SDL_oldnames.h>
+#include <SDL3/SDL_rect.h>
+#include <SDL3/SDL_render.h>
+#include <SDL3/SDL_timer.h>
+#include <SDL3/SDL_video.h>
+#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define FRAME_RATE 60
+
+#define PI 3.14159265
+#define TWO_PI 6.28318530
+#define TILE_SIZE 64
+#define MAP_NUM_ROWS 13
+#define MAP_NUM_COLS 20
+#define WINDOW_WIDTH (MAP_NUM_COLS * TILE_SIZE)
+#define WINDOW_HEIGHT (MAP_NUM_ROWS * TILE_SIZE)
+
+#define MINIMAP_SCALE_FACTOR 0.3
+
+#define FOV_ANGLE (60 * (PI / 180))
+
+#define NUM_RAYS WINDOW_WIDTH
+
+typedef struct Player Player;
+
+struct Player {
+  float x;
+  float y;
+  float width;
+  float height;
+  float turnDirection;
+  float walkDirection;
+  float rotationAngle;
+  float walkSpeed;
+  float turnSpeed;
+};
+Player player = {
+    WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, 5, 5, 0, 0, PI / 2, 100,
+    45 * (PI / 180),
+};
+
+const int map[MAP_NUM_ROWS][MAP_NUM_COLS] = {
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
+
+SDL_Window *initializeWindow() {
+  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS) == false) {
+    fprintf(stderr, "Error initializing SDL %s\n", SDL_GetError());
+    exit(1);
+  }
+  SDL_Window *window = SDL_CreateWindow("Raycaster", WINDOW_WIDTH,
+                                        WINDOW_HEIGHT, SDL_WINDOW_BORDERLESS);
+  if (window == NULL) {
+    fprintf(stderr, "Error initializing SDL window %s\n", SDL_GetError());
+    exit(1);
+  }
+  return window;
+}
+
+SDL_Renderer *initializeRenderer(SDL_Window *window) {
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
+  if (renderer == NULL) {
+    fprintf(stderr, "Error initializing SDL renderer %s\n", SDL_GetError());
+    exit(1);
+  }
+  SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND);
+  return renderer;
+}
+
+void render_map(SDL_Renderer *renderer) {
+  for (int i = 0; i < MAP_NUM_ROWS; i++) {
+    for (int j = 0; j < MAP_NUM_COLS; j++) {
+      int tile_x = j * TILE_SIZE * MINIMAP_SCALE_FACTOR;
+      int tile_y = i * TILE_SIZE * MINIMAP_SCALE_FACTOR;
+      int tile_color = 255 * map[i][j];
+      SDL_SetRenderDrawColor(renderer, tile_color, tile_color, tile_color, 255);
+      SDL_FRect map_tile = {tile_x, tile_y, TILE_SIZE * MINIMAP_SCALE_FACTOR,
+                            TILE_SIZE * MINIMAP_SCALE_FACTOR};
+      SDL_RenderFillRect(renderer, &map_tile);
+    }
+  }
+}
+
+void render(SDL_Renderer *renderer) {
+  SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+  SDL_RenderClear(renderer);
+
+  SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
+  render_map(renderer);
+  SDL_RenderPresent(renderer);
+}
 
 int main(void) {
-  printf("nothing");
-  return 0;
+  SDL_Window *window = initializeWindow();
+  SDL_Renderer *renderer = initializeRenderer(window);
+
+  unsigned int last_frame_ticks = 0;
+  while (true) {
+    float delta_time = (SDL_GetTicks() - last_frame_ticks) / 1000.0;
+    if (delta_time < 1000.0 / FRAME_RATE) {
+      SDL_Delay(1000.0 / FRAME_RATE - delta_time);
+    }
+    SDL_Event event;
+    SDL_PollEvent(&event);
+    switch (event.type) {
+    case SDL_EVENT_KEY_DOWN:
+      if (event.key.scancode != SDL_SCANCODE_ESCAPE)
+        break;
+    case SDL_EVENT_QUIT:
+      SDL_DestroyRenderer(renderer);
+      SDL_DestroyWindow(window);
+      SDL_Quit();
+      return 0;
+    }
+    player.x++;
+    player.y++;
+    render(renderer);
+  }
 }
